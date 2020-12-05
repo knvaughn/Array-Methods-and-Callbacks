beab0ba9376916f5f4ec375a8c474402e6968def
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2020-12-04 22:13:27 -0500

    completed task 5

--- index.js
+++ index.js
@@ -93,11 +93,13 @@ Use the higher-order function getWinnersByYear to do the following:
 hint: the strings returned need to exactly match the string in step 4.
  */
 
-function getWinnersByYear(/* code here */) {
-    /* code here */
+function getWinnersByYear(arr, getYears, getWinners) {
+    let winners = getWinners(arr, getFinals);
+    let years = getYears(arr, getFinals);
+    return winners.map((country, index) => `In ${years[index]}, ${country} won the world cup!`);
 }
 
-
+console.log(getWinnersByYear(fifaData, getYears, getWinners));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher order function getAverageGoals to do the following: 
